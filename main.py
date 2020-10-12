47d17ccd2558b1787ebd0e9e0d4c62b765ce8ded
Elton <32944003+EltZ36@users.noreply.github.com>
2020-10-12 10:28:00 -0700

    Update main.py
    more setup and variables.

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import random
 
 def Card():
+  numCards = 52
+  # Note: there is 4 suits with 13 cards in each suit. 
   return none
 
 def Setup():
   print("Welcome to 21/Blackjack")
+  print(""" What would you like?
+            -P Vs. P 
+            -P Vs. AI """)
+  Dict = {}
+  while True:
+    try:
+      return none
+      break
+    except Keyerror:
+      continue
