780413f1ead73da75884bf953195f8d1a793a85e
Elton <32944003+EltZ36@users.noreply.github.com>
2020-10-18 13:15:42 -0700

    Adding player selection
    I have started to add choices for the player on whether or not to draw more or not. The 2nd option(not drawing) is not done yet. The card deck was changed from a number set to a list. I don't work on lists often so I had to look at some examples of card games in Python. I will try to make it look more readable and not have so many nested if statements as projects like these are prone to have.

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import random
 
 def Card():
-  numCards = 52
+  cardsDeck = [1,2,3,4,5,6,7,8,9,10,10,10,10,11]*4
   # Note: there is 4 suits with 13 cards in each suit. 
-  random.shuffle(numCards)
-  choices = input("Would you like to draw more (1) or hold onto your deck (2)? ")
-  print(choices)
+  #Options = {}
+  random.shuffle(cardsDeck)
+  pCards = []
+  dCards = []
+  while len(pCards) != 2:
+    pCards.append(cardsDeck.pop())
+    if len(pCards) == 2:
+      pSum = sum(pCards)
+      print("Player has "+str(pSum))
+  while pSum <= 21:
+    choices = int(input("Would you like to draw more (1) or hold onto your deck (2)? "))
+    if (choices == 1):
+      pCards.append(cardsDeck.pop())
+      pSum = sum(pCards)
+      print("Player has "+str(pSum))
+      if (pSum > 21):
+        print("Player loses.")
+        break
+      elif (pSum == 21):
+        print("Player wins")
+        break
 
 def Setup():
   print("Welcome to 21/Blackjack")
   print(""" What would you like?
             -P Vs. P 
             -P Vs. AI """)
-  Dict = {}
+  Dict = {"1":PvP()}
   while True:
     try:
-      return none
+      Dict[gamemode()]
       break
     except Keyerror:
       continue
